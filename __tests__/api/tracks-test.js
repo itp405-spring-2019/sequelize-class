d888fe51a945cafd1320a603bda28812fd4f32e7
David Tang <david.tang@verizondigitalmedia.com>
2019-03-28 22:22:51 -0700

    fix test

--- __tests__/api/tracks-test.js
+++ __tests__/api/tracks-test.js
@@ -5,7 +5,7 @@ const { Joi } = frisby;
 it('should return a status of 200', function () {
   return frisby
     .get('http://localhost:8000/api/tracks/5')
-    .expect('status', 201);
+    .expect('status', 200);
 });
 
 it('should return a status of 404 when the track is not found', function () {
