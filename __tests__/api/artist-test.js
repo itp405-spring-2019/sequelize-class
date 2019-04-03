a44361c20544fc516b8dc4c6c618e2f479a7fc56
David Tang <david.tang@verizondigitalmedia.com>
2019-04-02 19:39:07 -0700

    add a bunch of tests and setup Travis CI

---
+++ __tests__/api/artist-test.js
@@ -0,0 +1,13 @@
+const frisby = require('frisby');
+
+const { Joi } = frisby;
+
+it('should create an artist', () => {
+  // return frisby
+  //   .post('http://localhost:8000/api/artists', {
+  //     name: 'ITP'
+  //   })
+  //   .expect('status', 200)
+  //   .expect('json', 'name', 'ITP')
+  //   .expect('jsonTypes', 'id', Joi.number().required());
+});
