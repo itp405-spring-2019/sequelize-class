e8f1efa6a4c34d2861d8c4f5d25a73129a15bd2e
David Tang <david.tang@verizondigitalmedia.com>
2019-04-02 19:47:44 -0700

    adds back POST and DELETE tests

--- __tests__/api/artist-test.js
+++ __tests__/api/artist-test.js
@@ -3,11 +3,11 @@ const frisby = require('frisby');
 const { Joi } = frisby;
 
 it('should create an artist', () => {
-  // return frisby
-  //   .post('http://localhost:8000/api/artists', {
-  //     name: 'ITP'
-  //   })
-  //   .expect('status', 200)
-  //   .expect('json', 'name', 'ITP')
-  //   .expect('jsonTypes', 'id', Joi.number().required());
+  return frisby
+    .post('http://localhost:8000/api/artists', {
+      name: 'ITP'
+    })
+    .expect('status', 200)
+    .expect('json', 'name', 'ITP')
+    .expect('jsonTypes', 'id', Joi.number().required());
 });
--- __tests__/api/playlist-test.js
+++ __tests__/api/playlist-test.js
@@ -1,10 +1,10 @@
 const frisby = require('frisby');
 
-// it('should return a 204 when deleting a playlist that exists', () => {
-//   return frisby
-//     .del('http://localhost:8000/api/playlists/6')
-//     .expect('status', 204);
-// });
+it('should return a 204 when deleting a playlist that exists', () => {
+  return frisby
+    .del('http://localhost:8000/api/playlists/6')
+    .expect('status', 204);
+});
 
 it('should return a 404 when deleting a playlist that does not exist', () => {
   return frisby
